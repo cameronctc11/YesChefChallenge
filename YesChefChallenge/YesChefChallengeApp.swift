726cdae1bfa29891feb94752d8d1ae584a8b7911
cameronmckenzie <cameronctc11@gmail.com>
2025-10-01 14:27:25 -0600

    Initial Commit

---
+++ YesChefChallenge/YesChefChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesChefChallengeApp.swift
+//  YesChefChallenge
+//
+//  Created by McKenzie, Cameron - Student on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct YesChefChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YesChefChallengeTests/YesChefChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YesChefChallengeTests.swift
+//  YesChefChallengeTests
+//
+//  Created by McKenzie, Cameron - Student on 10/1/25.
+//
+
+import Testing
+@testable import YesChefChallenge
+
+struct YesChefChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
