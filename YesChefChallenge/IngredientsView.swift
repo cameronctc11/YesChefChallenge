d85ba8ada3e012bd45d8c62b26ac410f1fd7d4cb
cameronmckenzie <cameronctc11@gmail.com>
2025-10-02 14:44:27 -0600

    Finished Yes Chef App. Did MVP and + 2 Stretches.

---
+++ YesChefChallenge/IngredientsView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientsView.swift
+//  YesChefChallenge
+//
+//  Created by McKenzie, Cameron - Student on 10/1/25.
+//
+
+import SwiftUI
+
+struct IngredientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientsView()
+}
---
+++ YesChefChallenge/InstructionsView.swift
@@ -0,0 +1,18 @@
+//
+//  InstructionsView.swift
+//  YesChefChallenge
+//
+//  Created by McKenzie, Cameron - Student on 10/1/25.
+//
+
+import SwiftUI
+
+struct InstructionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InstructionsView()
+}
